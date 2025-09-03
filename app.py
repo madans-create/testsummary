00b37f674a45ae50289f09097284f3c23094ef26
Andrew2712 <157894329+Andrew2712@users.noreply.github.com>
2025-09-03 20:52:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import nltk
+nltk.download("punkt")
 import streamlit as st
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
@@ -76,3 +78,4 @@ with col2:
 # -----------------------------
 st.markdown("---")
 st.markdown("© 2025 Text Summarizer App | Built with Python & Streamlit 💻")
+
